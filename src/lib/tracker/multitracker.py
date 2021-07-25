ff46d6a3df52bb4b12f67343e775619f39a697cc
Pinganan <46879811+Pinganan@users.noreply.github.com>
2021-07-25 23:13:09 +0800

    separate find detection and run tracker
    將原本偵測跟追蹤寫再一起的程式碼區隔開來
    就可以把兩個影片偵測完再追蹤

--- src/lib/tracker/multitracker.py
+++ src/lib/tracker/multitracker.py
@@ -224,14 +224,8 @@ class JDETracker(object):
                 keep_inds = (results[j][:, 4] >= thresh)
                 results[j] = results[j][keep_inds]
         return results
-
-    def update(self, im_blob, img0):
-        self.frame_id += 1
-        activated_starcks = []
-        refind_stracks = []
-        lost_stracks = []
-        removed_stracks = []
-
+    
+    def get_detection(self, im_blob, img0):
         width = img0.shape[1]
         height = img0.shape[0]
         inp_height = im_blob.shape[2]
@@ -281,6 +275,15 @@ class JDETracker(object):
                           (tlbrs, f) in zip(dets[:, :5], id_feature)]
         else:
             detections = []
+        print()
+        return detections
+
+    def update(self, detections):
+        self.frame_id += 1
+        activated_starcks = []
+        refind_stracks = []
+        lost_stracks = []
+        removed_stracks = []
 
         ''' Add newly detected tracklets to tracked_stracks'''
         unconfirmed = []
